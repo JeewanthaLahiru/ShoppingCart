ccdd9d955eae6b492df7c9a5514f1725be3e8e36
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-09-28 20:37:17 +0530

    successfully created a responsive navbar to homepage

--- index.php
+++ index.php
@@ -3,11 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/index.css">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+    <div class="navBar" id="navBarId">
+        <ul>
+            <li class="iconLi"><a href="javascript:void(0);" class="icon" onclick="responsiveNavBar()"><i class="fa fa-bars" aria-hidden="true"></i></a></li>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </div>
     <a href="login.php">login</a>
     <a href="signup.php">signup</a>
+
+    <script src="scripts/index.js"></script>
 </body>
 </html>
---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+function responsiveNavBar(){
+    var x = document.getElementById("navBarId");
+    if(x.className === "navBar"){
+        x.className += " responsive";
+    }else{
+        x.className = "navBar";
+    }
+}
