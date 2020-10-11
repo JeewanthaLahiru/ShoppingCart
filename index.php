587d089c70b0178b79f6bef89169eec3627ed0af
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-11 14:20:35 +0530

    changed the title to modifylk

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="styles/index2.css?v=<?php echo time(); ?>">
-    <title>Document</title>
+    <title>ModifyLK</title>
 </head>
 <body>
     <div class="navBar" id="navBarId">
