8cf1c8b9cc51bc2bad32446e35c9ab43f4cc7710
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-19 22:15:32 +0530

    created style for register page

--- register.php
+++ register.php
@@ -93,6 +93,7 @@ if(empty(trim($_POST["username"]))){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/register.css">
     <title>Document</title>
 </head>
 <body>
